81064f6756cf13c26f3b43591338ec3516cf35f0
ClawsPT <claws@sapo.pt>
2024-09-22 11:23:28 +0100

    Update

--- meoConnect.sh
+++ meoConnect.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version='0.502'
+version='0.503'
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 confFile=$HOME/.config/meoConnect/${0##*/}.conf
@@ -791,7 +791,7 @@ while true ; do
 				cat -b $HOME/.config/meoConnect/${0##*/}.lst
 				read -p "connect to: " lineNumber
 				bssid=$(sed -n "$lineNumber"p $HOME/.config/meoConnect/${0##*/}.lst)
-				echo $(echo $bssid | cut -c1-17
+				echo $(echo $bssid | cut -c1-17)
 
 			echo "------------------------------- TESTE -----------------------------------------"
 # ----------------------------------------------- TESTE -----------------------------------------

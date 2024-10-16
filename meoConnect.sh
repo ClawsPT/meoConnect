cd3d0ade75728a0104ca1cbc59db03bffa62083d
ClawsPT <claws@sapo.pt>
2024-10-16 23:40:34 +0100

    Update

--- meoConnect.sh
+++ meoConnect.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version='0.541'
+version='0.542'
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 confFile="$HOME/.config/meoConnect/${0##*/}.conf"
@@ -201,8 +201,8 @@ vpnDisconnect () {
 connectMeoWiFi () {
 		if [ "$connectionVer" == "v2" ] ; then
 			echo "Reconnecting to $(iwconfig $wifiif | sed -n 's/.*Access Point: \([0-9\:A-F]\{17\}\).*/\1/p')"
-			echo $rPasswd | sudo -S ifconfig $wifiif down > /dev/null 2>&1
-			echo $rPasswd | sudo -S ifconfig $wifiif up > /dev/null 2>&1
+			#echo $rPasswd | sudo -S ifconfig $wifiif down > /dev/null 2>&1
+			#echo $rPasswd | sudo -S ifconfig $wifiif up > /dev/null 2>&1
 			nmcli connection up "$wifiap" ifname "$wifiif" > /dev/null 2>&1
 		fi
 		
@@ -662,7 +662,7 @@ echo -e "-----------------------------------------------------------------------
 
 clear
 echo "-------------------------------------------------------------------------------"
-echo "|                        MEO Wifi AutoConnect v$version                     grep     |"
+echo "|                        MEO Wifi AutoConnect v$version                          |"
 echo "-------------------------------------------------------------------------------"
 startUp
 
@@ -826,7 +826,7 @@ while true ; do
 			echo "------------------------------- TESTE -----------------------------------------"
 
 			
-				syncTime
+				nmcli connection up "$wifiap" ifname "$wifiif" > /dev/null 2>&1
  
  
 			echo "------------------------------- TESTE -----------------------------------------"

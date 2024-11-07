7aa0e6265f61eb5992f1dfadeb937381214e79b3
ClawsPT <111670695+ClawsPT@users.noreply.github.com>
2024-11-07 23:59:05 +0000

    Add files via upload

--- meoConnect.sh
+++ meoConnect.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version='0.546'
+version='0.547'
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 confFile="$HOME/.config/meoConnect/${0##*/}.conf"
@@ -801,11 +801,12 @@ while true ; do
 				echo -n "Scanning WiFi networks: " 				
 				echo $rPasswd | sudo -S ifconfig $wifiif up > /dev/null 2>&1
 				echo $rPasswd | sudo -S nmcli --fields SSID,BSSID,CHAN,BARS,SIGNAL device wifi list ifname $wifiif --rescan yes | grep "MEO-WiFi" | sort > $HOME/.config/meoConnect/${0##*/}.lst
-				sed -i 's/MEO-WiFi//g' $HOME/.config/meoConnect/${0##*/}.lst
-				sed -i 's/ //g' $HOME/.config/meoConnect/${0##*/}.lst
+				sed -i 's/MEO-WiFi                //g' $HOME/.config/meoConnect/${0##*/}.lst
+				#sed -i 's/  //g' $HOME/.config/meoConnect/${0##*/}.lst
 				echo -e "\033[1;92mDone.\033[0m $(wc -l < $HOME/.config/meoConnect/${0##*/}.lst) APs found."
 	
-			# Connecting to BSSID list.	
+			# Connecting to BSSID list.
+				echo "     # |      BSSID       |Chan| Signal   "
 				cat -b $HOME/.config/meoConnect/${0##*/}.lst
 				read -p "connect to: " lineNumber
 				

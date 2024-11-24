3c17c420c16ab9682c0244ba90b83a13586b8a95
ClawsPT <claws@sapo.pt>
2024-11-24 15:33:43 +0000

    Update

--- meoConnect.sh
+++ meoConnect.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version='0.552'
+version='0.553'
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 confFile="$HOME/.config/meoConnect/${0##*/}.conf"
@@ -759,7 +759,7 @@ while true ; do
 		
 		echo "-------------------------------------------------------------------------------"
 		echo -e "    \033[1;91mOFFLINE\033[0m - $(date "+%H:%M:%S") | $connectionVer | T:$(printf "%02d" $(($(date --date """$(date "+%Y-%m-%d %H:%M:%S")""" +%s) - $currenttime))) | $(date -d "1970-01-01 + $totaltime seconds" "+%H:%M:%S")"
-		mpg321 -q $OfflineFile
+		mpg321 $OfflineFile > /dev/null 2>&1 &
 		echo "-------------------------------------------------------------------------------"
 		forceSynctime=1
 		vpnDisconnect

da9382ecfbfc0690e144a79a01d4b4fc52218f32
ClawsPT <claws@sapo.pt>
2025-02-26 22:17:10 +0000

    Update

--- meoConnect.sh
+++ meoConnect.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version='0.577'
+version='0.578'
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 confFile="$HOME/.config/meoConnect/${0##*/}.conf"
@@ -527,7 +527,7 @@ checkUpdate () {
 	gitVer=$(timeout --preserve-status -k 5 15 curl -H "Cache-Control: no-cache, no-store, must-revalidate, Pragma: no-cache, Expires: 0" https://raw.githubusercontent.com/ClawsPT/meoConnect/main/meoConnect.sh -s -r 13-28 | grep "version")
 	if [ "$gitVer" ] ; then
 		if [ "$gitVer" == "version='$version'" ] ; then
-			echo -e "\033[1;92mUpdated.\033[0m ($gitVer)"
+			echo -e "\033[1;92mUp-to-date.\033[0m ($gitVer)"
 		else
 			echo -e "\033[1;92mUpdate found.\033[0m ($gitVer)"
 			echo -e "Downloading update."

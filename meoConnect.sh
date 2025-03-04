5d1fa562b6b72cb43e6294b018f6e6d37470c568
ClawsPT <claws@sapo.pt>
2025-03-04 14:29:18 +0000

    Update

--- meoConnect.sh
+++ meoConnect.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version='0.584'
+version='0.585'
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 confFile="$HOME/.config/meoConnect/${0##*/}.conf"
@@ -615,7 +615,7 @@ while true ; do
 		netStatus=$(printf "$netStatus" | sed 's/\r//g' | sed 's/HTTP\/1.1 //g' | sed 's/HTTP\/1.0 //g')
 		if [[ $(echo $netStatus | grep "Moved") ]]; then #Moved -> redirected to login portal
 			echo "-------------------------------------------------------------------------------"
-			echo -e "    \033[1;91mOFFLINE\033[0m - $(date "+%H:%M:%S") - \033[1;92mRedirected to login portal\033[0m - $netStatus"
+			echo -e "\033[1;91m------ OFFLINE ------\033[0m  | $(date "+%H:%M:%S") | \033[1;92mRedirected to login portal\033[0m - $netStatus"
 			echo "-------------------------------------------------------------------------------"
 			mpg321 $OfflineFile > /dev/null 2>&1
 			connectMeoWiFi
@@ -667,7 +667,7 @@ while true ; do
 # -------------------------------------- OFFLINE ------------------------------------
 		
 		echo "-------------------------------------------------------------------------------"
-		echo -e "    \033[1;91mOFFLINE\033[0m - $(date "+%H:%M:%S") | $connectionVer | T:$(printf "%02d" $(($(date --date """$(date "+%Y-%m-%d %H:%M:%S")""" +%s) - $currenttime))) | $(date -d "1970-01-01 + $totaltime seconds" "+%H:%M:%S")"
+		echo -e "\033[1;91m------ OFFLINE ------\033[0m  | $(date "+%H:%M:%S") | $connectionVer | T:$(printf "%02d" $(($(date --date """$(date "+%Y-%m-%d %H:%M:%S")""" +%s) - $currenttime))) | $(date -d "1970-01-01 + $totaltime seconds" "+%H:%M:%S")"
 		mpg321 $OfflineFile > /dev/null 2>&1
 		echo "-------------------------------------------------------------------------------"
 		forceSynctime=1

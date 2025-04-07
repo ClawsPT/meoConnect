121541da054ff8f644368d8779fe5588570bed7d
ClawsPT <claws@sapo.pt>
2025-04-07 21:55:15 +0100

    Update

--- meoConnect.sh
+++ meoConnect.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version='0.597 e três quartos'
+version='0.598'
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 confFile="$HOME/.config/meoConnect/${0##*/}.conf"

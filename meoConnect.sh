2e0c5352f231b4a507db4cbaef5b16e5f3999731
ClawsPT <claws@sapo.pt>
2025-10-25 03:21:22 +0100

    Update

--- meoConnect.sh
+++ meoConnect.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version='0.677'
+version='0.678'
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 confFile="$HOME/.config/meoConnect/${0##*/}.conf"
@@ -260,6 +260,9 @@ syncTime () {
 		starttime=$(date -d "$meoTime Z" +%s)
 		currenttime=$(date --date """$(date "+%H:%M:%S")""" +%s)
 		totaltime=$(($currenttime - $starttime))
+		if [ $totaltime -lt 0 ] ; then 
+			totaltime=$(($totaltime + 86400))
+		fi
 		echo -e "\033[1;92m $(date -d "1970-01-01 + $totaltime seconds" "+%H:%M:%S")\033[0m"
 		XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send  "Successfully connected to MEO WiFi"
 		echo -e "Session ID             : \033[1;92m$sessionId\033[0m"		
@@ -267,9 +270,13 @@ syncTime () {
 		echo $($OLCmd $(iwconfig $wifiif | sed -n 's/.*Access Point: \([0-9\:A-F]\{17\}\).*/\1/p'))
 		echo -e "\033[0m                       : \033[1;92mDone.\033[0m"
 		echo "-----------------------:-------------------------------------------------------"
-		if [[ "$totaltime" -lt 7200 ]] ; then
+		echo "false $totaltime"
+		
+		if [ "$totaltime" -lt 7200 ] ; then
+			echo "false $totaltime"
 			Skip2h=false
 		else
+			echo "truw $totaltime"
 			Skip2h=true
 		fi
 	else
